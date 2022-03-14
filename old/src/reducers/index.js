683687e8a71f5e0d30374db33a8f05248feab30a
edwardlanto <edwardlanto.developer@gmail.com>
2022-03-14 00:17:05 -0700

    Moved next project to root

--- old/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import weatherReducer  from './reducer_weather';
-
-const rootReducer = combineReducers({
-  weather:weatherReducer
-});
-
-export default rootReducer;
