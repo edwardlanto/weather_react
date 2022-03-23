3668fdde55f5f09247739e595ebba27a9ad507c3
edwardlanto <edwardlanto.developer@gmail.com>
2022-03-22 23:12:20 -0700

    Commit

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  env:{
-    'OPEN_WEATHER_API': '89ab4def99148ab9d94cf4569a1f82f3',
-    'GOOGLE_API': 'AIzaSyBNEVncKNdJsiP6n3XWlSSc3ojfWVahGVY'
-  }
-}
